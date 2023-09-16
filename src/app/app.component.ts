fcd39f9042f5095d2d59a16883f4add547980567
Administrator <lari.farshad@gmail.com>
2023-09-17 00:32:07 +0330

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular4app';
+
+  @ViewChild('slick') slick!: any;
+  index = 0;
+  slides = [
+    { img: "http://placehold.it/350x150/000000" },
+    { img: "http://placehold.it/350x150/111111" },
+    { img: "http://placehold.it/350x150/333333" },
+    { img: "http://placehold.it/350x150/666666" }
+  ];
+  slideConfig = { "slidesToShow": 1, "slidesToScroll": 1 };
+
+
+  removeSlide() {
+    this.slides.length = this.slides.length - 1;
+  }
+
+  slickInit(e: any) {
+  }
+
+  breakpoint(e: any) {
+  }
+
+  afterChange(e: any) {
+    // this.index += 1;
+  }
+
+  beforeChange(e: any) {
+    this.index = e.nextSlide;
+  }
+  changeImage(event: any) {
+    this.slick
+    this.index = +event.target.id;
+  }
 }
